8c222d61b1f05824592bfca14cfd8da73dc8d0c0
alvin4104 <stewlandnguyen313@gmail.com>
2024-12-19 12:43:26 +0700

    Create SensorDataReducer.java

---
+++ home/alvinnguyen41/Alvin04/src/main/java/org/sensor/SensorDataReducer.java
@@ -0,0 +1,22 @@
+package org.sensor;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import java.io.IOException;
+
+public class SensorDataReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+    private IntWritable result = new IntWritable();
+
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+        int sum = 0;
+        int count = 0;
+        for (IntWritable val : values) {
+            sum += val.get();
+            count++;
+        }
+        result.set(sum / count); // Average temperature
+        context.write(key, result);
+    }
+}
